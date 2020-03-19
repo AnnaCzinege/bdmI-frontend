379f6f54ac0869c2b3a32dd10918accf627aa0fd
AnnaCzinege <42236372+AnnaCzinege@users.noreply.github.com>
2020-03-19 16:25:21 +0100

    Chage link tag in WatchList.js to Button tag

--- imdb/src/components/WatchList.js
+++ imdb/src/components/WatchList.js
@@ -1,7 +1,7 @@
-import React, { useContext } from 'react';
-import { Table, Popconfirm } from 'antd';
-import { StarTwoTone, DeleteFilled } from '@ant-design/icons';
-import { WatchListContext } from './WatchListContext';
+import React, { useContext } from "react";
+import { Table, Popconfirm, Button } from "antd";
+import { StarTwoTone, DeleteFilled } from "@ant-design/icons";
+import { WatchListContext } from "./WatchListContext";
 
 const WatchList = props => {
   const { moviesToWatch, setMoviesToWatch } = useContext(WatchListContext);
@@ -15,23 +15,23 @@ const WatchList = props => {
 
   const columns = [
     {
-      title: 'Title',
-      dataIndex: 'title'
+      title: "Title",
+      dataIndex: "title"
     },
     {
-      title: 'Poster',
-      dataIndex: 'poster_path',
+      title: "Poster",
+      dataIndex: "poster_path",
       render: text => (
         <img
-          style={{ height: '120px', width: '80px' }}
+          style={{ height: "120px", width: "80px" }}
           src={`https://image.tmdb.org/t/p/w500${text}`}
           alt="poster"
         />
       )
     },
     {
-      title: 'Vote average',
-      dataIndex: 'vote_average',
+      title: "Vote average",
+      dataIndex: "vote_average",
       render: text => (
         <React.Fragment>
           <StarTwoTone twoToneColor="#ebe82f" />
@@ -40,33 +40,33 @@ const WatchList = props => {
       )
     },
     {
-      title: 'Release date',
-      dataIndex: 'release_date'
+      title: "Release date",
+      dataIndex: "release_date"
     },
     {
-      title: 'OverView',
-      dataIndex: 'overview'
+      title: "OverView",
+      dataIndex: "overview"
     },
     {
-      title: 'Delete',
-      dataIndex: 'id',
+      title: "Delete",
+      dataIndex: "id",
       render: (text, record) =>
         moviesToWatch.length >= 1 ? (
           <Popconfirm
             title="Sure to delete?"
             onConfirm={() => handleDelete(text)}
           >
-            <a>
+            <Button>
               <DeleteFilled />
               {record.key}
-            </a>
+            </Button>
           </Popconfirm>
         ) : null
     }
   ];
 
   return (
-    <div style={{ marginTop: '50px' }}>
+    <div style={{ marginTop: "50px" }}>
       <Table columns={columns} dataSource={moviesToWatch} />
     </div>
   );
