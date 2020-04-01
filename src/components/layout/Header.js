872f65c14b437bf0c7c96ac103d45a0072322529
AnnaCzinege <42236372+AnnaCzinege@users.noreply.github.com>
2020-04-01 10:06:10 +0200

    Remove unnecessary imports from Header.js

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -4,13 +4,10 @@ import { LayoutContext } from "./LayoutContext";
 import { SearchMoviesContext } from "../SearchMoviesContext";
 import ToggleBtn from "../../ToggleBtn.png";
 import Logo from "../../Logo.png";
-import SearchIcon from "../../SearchIcon.png";
 import StyledHeader from "../elements/header_elements/HeaderStyle";
 import StyledHeaderItem from "../elements/header_elements/HeaderItemStyle";
 import StyledLogo from "../elements/header_elements/HeaderLogoStyle";
 import StyledToggleBtn from "../elements/header_elements/HeaderToggleBtnStyle";
-//import StyledInput from "../elements/header_elements/HeaderInputStyle";
-import StyledSearchIcon from "../elements/header_elements/HeaderSearchIconStyle";
 import { AutoComplete } from "antd";
 import StyledLink from "../elements/header_elements/HeaderLinkStyle";
 
