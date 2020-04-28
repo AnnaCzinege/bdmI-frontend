492486bb5d7daff49322539c12d675ab55a7d959
AnnaCzinege <42236372+AnnaCzinege@users.noreply.github.com>
2020-04-28 13:47:29 +0200

    Remove unused react hook

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useEffect, useContext } from "react";
 import { MovieContext } from "./contexts/MovieContext";
 import { Layout, Typography, Card, Col, Row, Rate, message } from "antd";
 import {
