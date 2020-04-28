c56ae611676bd1b417093e24165fae9b5eb7dfae
Rasgacode <rongybaba12@gmail.com>
2020-04-28 14:21:11 +0200

    implement login, logout userMessages

--- src/components/contexts/UserContext.js
+++ src/components/contexts/UserContext.js
@@ -56,27 +56,39 @@ export const UserProvider = (props) => {
   };
 
   const logInUser = (User) => {
-    Axios.post("https://localhost:44314/api/user/login", User).then((resp) => {
-      if (resp.data.errorMessage === "Username or password was invalid") {
-        message.warning(resp.data.errorMessage, 1);
-      } else {
-        new Cookies().set("c_user", resp.data);
-        checkCookiesForLog();
-      }
-    }); //TODO
+    Axios.post("https://localhost:44314/api/user/login", User)
+      .then((resp) => {
+        if (resp.status === 200) {
+          new Cookies().set("c_user", resp.data);
+          checkCookiesForLog();
+        }
+      })
+      .catch((error) => {
+        switch (error.response.status) {
+          case 400:
+            message.warning("Username or password was invalid!", 5);
+            break;
+          default:
+            message.warning("Cannot connect to bDMI database", 5);
+            break;
+        }
+      });
   };
 
   const logOutUser = () => {
     const cookies = new Cookies();
-    Axios.post(
-      "https://localhost:44314/api/user/logout",
-      cookies.get("c_user")
-    ).then((resp) => {
-      if (resp.data === "You have been logged out") {
+    Axios.post("https://localhost:44314/api/user/logout", cookies.get("c_user"))
+      .then((resp) => {
+        if (resp.status === 200) {
+          cookies.remove("c_user");
+          checkCookiesForLog();
+        }
+      })
+      .catch((error) => {
         cookies.remove("c_user");
         checkCookiesForLog();
-      }
-    });
+        message.warning("Cannot connect to bDMI database", 5);
+      });
   };
 
   const addMovieToWatchList = (event, properties) => {
